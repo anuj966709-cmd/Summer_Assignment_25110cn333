bbadbc4cca6ff432570cea2549e9c3df1b5a3037
anuj966 <anuj966709@gmail.com>
2026-06-30 11:58:31 +0530

    Complete Day 8 assignment

---
+++ day8/Question29.java
@@ -0,0 +1,21 @@
+//Write a program to print half pyramid pattern;
+package day8;
+
+import java.util.Scanner;
+public class Question29 {
+      public static void main(String[] args)
+      {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter the value of n: ");
+            int n = sc.nextInt();
+            for(int i = 1; i <= n; i++)
+            {
+                  for(int j = 1; j <= i; j++)
+                  {
+                        System.out.print("* ");
+                  }
+                  System.out.println();
+            }
+      }
+      
+}
---
+++ day8/Question32.java
@@ -0,0 +1,23 @@
+//Write a program to print repeated number.
+package day8;
+
+import java.util.Scanner;
+public class Question32 {
+      public static void main(String[] args)
+      {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter the value of n: ");
+            int n = sc.nextInt();
+            int a = 1;
+            for(int i = 1; i <= n; i++)
+            {
+                  for(int j = 1; j <= i; j++)
+                  {
+                        System.out.print(a + " ");
+                  }
+                  a++;
+                  System.out.println();
+            }
+      }
+      
+}
